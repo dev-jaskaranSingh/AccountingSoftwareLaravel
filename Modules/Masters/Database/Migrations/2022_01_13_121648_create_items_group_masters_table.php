bcfd17de747aa36dfc9fcc794c3bf7d21fb71f52
Jaskaran Singh <42887065+JaskaranSingh4704@users.noreply.github.com>
2022-01-14 22:01:53 +0530

    fixed migrations issue

--- Modules/Masters/Database/Migrations/2022_01_13_121648_create_items_group_masters_table.php
+++ Modules/Masters/Database/Migrations/2022_01_13_121648_create_items_group_masters_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateItemsGroupMasterTable extends Migration
+class CreateItemsGroupMastersTable extends Migration
 {
     /**
      * Run the migrations.
--- Modules/Masters/Database/Migrations/2022_01_14_162718_create_hsn_master_table.php
+++ Modules/Masters/Database/Migrations/2022_01_14_162718_create_hsn_master_table.php
@@ -14,7 +14,7 @@ class CreateHsnMasterTable extends Migration
     public function up()
     {
         Schema::create('hsn_master',function (Blueprint $table){
-            $table->increments('id');
+            $table->id();
             $table->string('hsn_code');
             $table->string('hsn_description');
             $table->string('min_amount');
