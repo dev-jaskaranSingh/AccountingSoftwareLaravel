728b848eb87dbd967daca666b6bc4bd821678ef8
Jaskaran Singh <42887065+JaskaranSingh4704@users.noreply.github.com>
2022-01-24 14:50:36 +0530

    setup handsone table

--- Modules/Transactions/Resources/views/purchases/_form.blade.php
+++ Modules/Transactions/Resources/views/purchases/_form.blade.php
@@ -16,13 +16,55 @@
                         @enderror
                     </div>
                 </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div id="handsontable"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
-
-
 @section('scripts')
+    <script>
+        const data = [];
+
+        const container = document.getElementById('handsontable');
+        const hot = new Handsontable(container, {
+        data: data,
+            colWidths: [140, 126, 192, 100, 100, 90, 90, 110, 97],
+            colHeaders: [
+                "Item",
+                "HSN",
+                "Unit",
+                "Gross WT",
+                "Net WT",
+                "Rate",
+                "Qty",
+                "Amount",
+            ],
+            columns: [
+                { data: 1, type: "text" },
+                { data: 2, type: "text" },
+                { data: 3, type: "text" },
+                { data: 4, type: "numeric"},
+                { data: 5, type: "numeric", className: "htMiddle"},
+                { data: 6, type: "numeric", readOnly: true, className: "star htCenter"},
+                { data: 7, type: "numeric", className: "htMiddle"},
+                { data: 8, type: "numeric", readOnly: true, className: "star htCenter"}
+            ],
+            dropdownMenu: false,
+            hiddenColumns: {
+                indicators: true
+            },
+            contextMenu: true,
+            multiColumnSorting: true,
+            filters: true,
+            rowHeaders: true,
+            manualRowMove: true,
+            licenseKey: "non-commercial-and-evaluation"
+        });
+    </script>
 
 @endsection
