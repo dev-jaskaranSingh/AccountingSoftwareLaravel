2352166e772c677fd84263a66b448d783cb87784
sandeep514 <sandy.singh51480@gmail.com>
2022-02-10 15:31:49 +0530

    create stock master curd

---
+++ Modules/Transactions/Repositories/PurchaseRepository.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Modules\Transactions\Repositories;
+
+class PurchaseRepository
+{
+
+}
---
+++ Modules/Transactions/Services/SaleServices.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Modules\Transactions\Services;
+
+class SaleServices
+{
+
+}
