c014f315a4d1c04a515e69b527fb51a5d70524e9
Jaskaran Singh <jaskaransingh4704@gmail.com>
2022-03-23 12:52:28 +0530

    udpate table view

--- Modules/Reports/Resources/views/trial-balance/index.blade.php
+++ Modules/Reports/Resources/views/trial-balance/index.blade.php
@@ -56,8 +56,6 @@
                                 @endforeach
                                 </tbody>
                             </table>
-
-                            {{--                            {!! $dataTable->table(['class'=>'table table-striped table-bordered table-hover']) !!}--}}
                         </div>
                     </div>
                 </div>
@@ -68,13 +66,5 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.0.3/css/buttons.dataTables.min.css">
     <script src="https://cdn.datatables.net/buttons/1.0.3/js/dataTables.buttons.min.js"></script>
     <script src="{{ asset('vendor/datatables/buttons.server-side.js')}}"></script>
-    {{--    {!! $dataTable->scripts() !!}--}}
-    {{--    <script type="text/javascript">--}}
-    {{--        $(document).on('click', '.delete-row', function () {--}}
-    {{--            if (confirm('Are you sure to delete this record?')) {--}}
-    {{--                $(this).parent('form').submit();--}}
-    {{--            }--}}
-    {{--        });--}}
-    {{--    </script>--}}
 @endsection
 @endsection
